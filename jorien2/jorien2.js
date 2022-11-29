111eed261f5b27ceb49b50719e5e950c7b42300e
neilPintoGitHub <94918865+neilPintoGitHub@users.noreply.github.com>
2022-11-29 18:44:44 +0100

    v20221129.1845

--- jorien2/jorien2.js
+++ jorien2/jorien2.js
@@ -2,7 +2,7 @@
 
 function updFooterVer()
 {
-  document.getElementById("footer-ver").innerText = "v20221129.1800." + window.innerWidth + "x" + window.innerHeight;
+  document.getElementById("footer-ver").innerText = "v20221129.1845." + window.innerWidth + "x" + window.innerHeight;
 }
 
 updFooterVer();
