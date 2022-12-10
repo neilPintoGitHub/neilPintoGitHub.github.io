048326023b930bdfcd7ec2a2db62a19af63e6949
neilPintoGitHub <94918865+neilPintoGitHub@users.noreply.github.com>
2022-12-10 10:12:20 +0100

    v20221210.1015

--- jorien2/jorien2.js
+++ jorien2/jorien2.js
@@ -2,7 +2,7 @@
 
 function updFooterVer()
 {
-  document.getElementById("jw-footer-tooltiptext").innerText = "v20221209.2300." + window.innerWidth + "x" + window.innerHeight;
+  document.getElementById("jw-footer-tooltiptext").innerText = "v20221210.1015." + window.innerWidth + "x" + window.innerHeight;
 }
 
 updFooterVer();
